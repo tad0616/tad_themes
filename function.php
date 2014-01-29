fc9f91b67b1c06bf1c92b030a2323e933ac2aa71
Tad <tad0616@gmail.com>
2014-01-29 14:27:34 +0800

    精簡不必要的函數

--- function.php
+++ function.php
@@ -16,19 +16,5 @@ include_once XOOPS_ROOT_PATH."/modules/tadtools/tad_function.php";
 
 
 /********************* 預設函數 *********************/
-//圓角文字框
-function div_3d($title="",$main="",$kind="raised",$style="",$other=""){
-	$main="<table style='width:auto;{$style}'><tr><td>
-	<div class='{$kind}'>
-	<h1>$title</h1>
-	$other
-	<b class='b1'></b><b class='b2'></b><b class='b3'></b><b class='b4'></b>
-	<div class='boxcontent'>
- 	$main
-	</div>
-	<b class='b4b'></b><b class='b3b'></b><b class='b2b'></b><b class='b1b'></b>
-	</div>
-	</td></tr></table>";
-	return $main;
-}
+
 ?>
