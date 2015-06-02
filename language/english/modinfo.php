2f49238d5961ff347cab89c771069355b59e7a4a
mambax7 <mambax7@gmail.com>
2015-06-02 08:10:08 -0400

    English Update

--- language/english/modinfo.php
+++ language/english/modinfo.php
@@ -1,20 +1,26 @@
 <?php
-include_once XOOPS_ROOT_PATH."/modules/tadtools/language/{$xoopsConfig['language']}/modinfo_common.php";
+include_once XOOPS_ROOT_PATH . "/modules/tadtools/language/{$xoopsConfig['language']}/modinfo_common.php";
 
-define("_MI_TADTHEMES_NAME","Theme admin");
-define("_MI_TADTHEMES_DESC","The module can be used to setup specific theme.");
-define("_MI_TADTHEMES_ADMENU1", "Theme configuration");
-define("_MI_TADTHEMES_ADMENU2", "Menu configuration");
-define("_MI_TADTHEMES_BNAME1","responsive slider");
-define("_MI_TADTHEMES_BDESC1","responsive slider block");
-define("_MI_TADTHEMES_AUTO_MENU","Main menu dropdown?");
-define("_MI_TADTHEMES_AUTO_MENU_DESC","Does display main menu dropdown?");
-define("_MI_TADTHEMES_SHOW_SITENAME","Does display site name?");
-define("_MI_TADTHEMES_SHOW_SITENAME_DESC","Does display site name?");
-define("_MI_TADTHEMES_BNAME2","Slide Out Navigation");
-define("_MI_TADTHEMES_BDESC2","Beautiful Slide Out Navigation");
-define("_MI_TADTHEMES_BNAME3","BootStrap Vertical Menu");
-define("_MI_TADTHEMES_BDESC3","BootStrap Vertical Menu");
-define("_MI_TADTHEMES_BNAME4","Vertical Menu");
-define("_MI_TADTHEMES_BDESC4","Vertical Menu");
-?>
+define('_MI_TADTHEMES_NAME', 'Tad Theme admin');
+define('_MI_TADTHEMES_DESC', 'The module can be used to setup specific theme.');
+define('_MI_TADTHEMES_ADMENU1', 'Theme configuration');
+define('_MI_TADTHEMES_ADMENU2', 'Menu configuration');
+define('_MI_TADTHEMES_BNAME1', 'Responsive slider');
+define('_MI_TADTHEMES_BDESC1', 'Responsive slider block');
+define('_MI_TADTHEMES_AUTO_MENU', 'Main menu Dropdown?');
+define('_MI_TADTHEMES_AUTO_MENU_DESC', 'Should main menu display Dropdown menu?');
+define('_MI_TADTHEMES_SHOW_SITENAME', 'Display site name?');
+define('_MI_TADTHEMES_SHOW_SITENAME_DESC', 'Should we display site name?');
+define('_MI_TADTHEMES_BNAME2', 'Slide Out Navigation');
+define('_MI_TADTHEMES_BDESC2', 'Beautiful Slide Out Navigation');
+define('_MI_TADTHEMES_BNAME3', 'Bootstrap Vertical Menu');
+define('_MI_TADTHEMES_BDESC3', 'Bootntrap Vertical Menu');
+define('_MI_TADTHEMES_BNAME4', 'Vertical Menu');
+define('_MI_TADTHEMES_BDESC4', 'Vertical Menu');
+
+define('_MI_TADTHEMES_DIRNAME', basename(dirname(dirname(__DIR__))));
+define('_MI_TADTHEMES_HELP_HEADER', __DIR__ . '/help/helpheader.html');
+define('_MI_TADTHEMES_BACK_2_ADMIN', 'Back to Administration of ');
+
+//help
+define('_MI_TADTHEMES_HELP_OVERVIEW', 'Overview');
